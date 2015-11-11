f67e6ce82ed4abb994c7bbfd9738e04d81b5b9c7
Adrian Meza <adrianm1@outlook.com>
2015-11-10 19:56:53 -0700

    Se agregaron comentarios al archivo profiles/views.py

--- sfotipy/profiles/views.py
+++ sfotipy/profiles/views.py
@@ -1,13 +1,18 @@
+#modelos del nucleo de django
 from django.shortcuts import render
 from django.views.generic import View
+#modelos propios
 from django.contrib.auth.models import User
 
+#Vista para la creacion de un usuario
 class CreateUserView(View):
 
     def get(self,request):
         return render(request,'profiles/create_user.html')
 
+#Vista para el login de un usuario
 class LoginUserView(View):
 
     def get(self,request):
-        return render(request,'profiles/login_user.html')        
+        return render(request,'profiles/login_user.html')
+        

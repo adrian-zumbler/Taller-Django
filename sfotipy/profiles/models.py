9b068365602175964614ad081300f3795957ad6e
Adrian Meza <adrianm1@outlook.com>
2015-11-10 19:31:55 -0700

    Se agrego el campo phone a la aplicacion profiles

--- sfotipy/profiles/models.py
+++ sfotipy/profiles/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profiles(models.Model):
+    phone = models.CharField(max_length=255,null=True)
     user = models.OneToOneField(User)
